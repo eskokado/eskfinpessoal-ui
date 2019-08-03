4a376e5335a81d7c96f13f0b7df9b820afc1c859
Edson Shideki Kokado <eskokado@gmail.com>
2019-08-03 09:20:52 -0300

    Formatando labels no Chart.JS

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, DecimalPipe } from '@angular/common';
 
 import { PanelModule } from 'primeng/panel';
 import { ChartModule } from 'primeng/chart';
@@ -19,6 +19,9 @@ import { SharedModule } from '../shared/shared.module';
 
     SharedModule,
     DashboardRoutingModule
+  ],
+  providers: [
+    DecimalPipe
   ]
 })
 export class DashboardModule { }
--- src/app/dashboard/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DecimalPipe } from '@angular/common';
+
 import { DashboardService } from '../dashboard.service';
 
 @Component({
@@ -12,8 +14,22 @@ export class DashboardComponent implements OnInit {
   lineChartData: any;
   barChartData: any;
 
+  options = {
+    tooltips: {
+      callbacks: {
+        label: (tooltipItem, data) => {
+          const dataset = data.datasets[tooltipItem.datasetIndex];
+          const valor = dataset.data[tooltipItem.index];
+          const label = dataset.label ? (dataset.label + ': ') : '';
+          return label + this.decimalPipe.transform(valor, '1.2-2');
+        }
+      }
+    }
+  };
+
   constructor(
-    private dashboardService: DashboardService
+    private dashboardService: DashboardService,
+    private decimalPipe: DecimalPipe
   ) { }
 
   ngOnInit() {
